43cec47a849ee4794f4fbc04d439b55041e7cf27
stellamarma <stellamarmat@gmail.com>
2025-01-13 18:05:06 +0200

    Create random something generator

--- 13.project(rng-movie)/wpetsname/js/app.js
+++ 13.project(rng-movie)/wpetsname/js/app.js
@@ -1,76 +1,110 @@
-const {createApp, reactive, computed}= Vue;
+const { createApp, reactive, computed } = Vue;
 
-const DEAFULT_STATE={
-    state:true,
-    inputMovie:'',
-    names:[],
-    error:'',
-    showError:false,
-    result:''
-}
+const DEFAULT_STATE = {
+    state: true,
+    inputMovie: '',
+    inputName: '',
+    begin: true,
+    categories: [],
+    names: [],
+    error: '',
+    showError: false,
+    result: ''
+};
+
 createApp({
-    setup(){
-        const data = reactive(DEAFULT_STATE)
+    setup() {
+        const data = reactive(DEFAULT_STATE);
 
+        // Προσθήκη κατηγορίας
+        const add = () => {
+            const category = data.inputName.trim();
+
+            if (data.category==='') {
+                window.alert("Please enter a category");
+            } else if (data.categories.includes(category)) {
+                window.alert("Category already exists");
+            } else {
+                data.categories.push(category);
+                data.inputName = '';
+                data.begin = false;
+                console.log(`Added category: ${category}`);
+            }
+        };
+
+        // Προσθήκη ταινίας στη λίστα
         const addMovieToList = () => {
             const userMovie = data.inputMovie.trim();
-            
-            if (!userMovie) {//because we dont want to have null value
-                window.alert("Sorry, no empty movie")
 
-            }else if(data.names.includes(userMovie)){// if its already excist
-                window.alert("Sorry,movies must be unique")
-            }else{//when its true
+            if (!userMovie) {
+                window.alert("Sorry, no empty movie");
+            } else if (data.names.includes(userMovie)) {
+                window.alert("Sorry, movies must be unique");
+            } else {
                 data.names.push(userMovie);
                 data.inputMovie = '';
                 console.log(data.names);
             }
-        }
+        };
 
-        const removeMovie = (index) =>{
-            data.names.splice(index,1);
+        // Αφαίρεση ταινίας από τη λίστα
+        const removeMovie = (index) => {
+            data.names.splice(index, 1);
+        };
 
-        }
-        //computed
-        const isReady= computed(()=>{
-            return data.names.length>1;
-        })
-        const getRandomName=()=>{
-            return data.names[Math.floor(Math.random()*data.names.length)];
-        }
-        const genearteResult=()=>{
+        // Έλεγχος αν η λίστα έχει πάνω από 1 ταινία
+        const isReady = computed(() => {
+            return data.names.length > 1;
+        });
+
+        // Επιλογή τυχαίας ταινίας
+        const getRandomName = () => {
+            return data.names[Math.floor(Math.random() * data.names.length)];
+        };
+
+        // Δημιουργία αποτελέσματος
+        const generateResult = () => {
             let rand = getRandomName();
-            data.result=rand;
-        }
-        const showResults=(()=>{
-            genearteResult()
-            data.state=false;
-            return data.state;
-        })
-        const restApp= ()=>{
-            data.state=true;
-            data.inputMovie='';
-            data.names=[];
-            data.error='';
-            data.showError=false;
-            data.result='';
 
-        }
+            if (data.result !== '') {
+                while (rand === data.result) rand = getRandomName();
+            }
+            data.result = rand;
+        };
 
-        const getNewResult=()=>{
+        // Εμφάνιση αποτελέσματος
+        const showResults = () => {
+            generateResult();
+            data.state = false;
+        };
 
-            return genearteResult();
-        }
+        // Επαναφορά εφαρμογής
+        const restApp = () => {
+            data.state = true;
+            data.inputMovie = '';
+            data.inputName = '';
+            data.begin = true;
+            data.categories = [];
+            data.names = [];
+            data.error = '';
+            data.showError = false;
+            data.result = '';
+        };
 
-        
-    return{
-        data,
-        addMovieToList,
-        removeMovie,
-        isReady,
-        showResults,
-        restApp,
-        getNewResult
+        // Νέο τυχαίο αποτέλεσμα
+        const getNewResult = () => {
+            generateResult();
+        };
+
+        return {
+            data,
+            add,
+            addMovieToList,
+            removeMovie,
+            isReady,
+            showResults,
+            restApp,
+            getNewResult
+        };
     }
-    }
-}).mount('#app')
+}).mount('#app');
