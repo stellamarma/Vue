ba791b95860dbe0c384e629c5b8a9cc56841be1a
stellamarma <stellamarmat@gmail.com>
2025-01-13 15:09:15 +0200

    Create a rng movie

---
+++ 13.project(rng-movie)/wpetsname/js/app.js
@@ -0,0 +1,42 @@
+const {createApp, reactive, computed}= Vue;
+
+const DEAFULT_STATE={
+    state:true,
+    inputMovie:'',
+    names:[],
+    error:'',
+    showError:false,
+    result:''
+}
+createApp({
+    setup(){
+        const data = reactive(DEAFULT_STATE)
+
+        const addMovieToList = () => {
+            const userMovie = data.inputMovie.trim();
+            
+            if (!userMovie) {//because we dont want to have null value
+                window.alert("Sorry, no empty movie")
+
+            }else if(data.names.includes(userMovie)){// if its already excist
+                window.alert("Sorry,movies must be unique")
+            }else{//when its true
+                data.names.push(userMovie);
+                data.inputMovie = '';
+                console.log(data.names);
+            }
+        }
+
+        const removeMovie = (index) =>{
+            data.names.splice(index,1);
+
+        }
+        
+    return{
+        data,
+        addMovieToList,
+        removeMovie,
+
+    }
+    }
+}).mount('#app')
