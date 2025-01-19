5e88f3302c7c64dfbeca966614741a63265f560a
stellamarma <stellamarmat@gmail.com>
2025-01-19 10:40:28 +0200

    Add local registration

--- 14.first/first/src/App.vue
+++ 14.first/first/src/App.vue
@@ -1,12 +1,20 @@
 <template>
- <app-header></app-header>
- <h1>{{name}}</h1>
- <app-footer></app-footer>
+
+  <div>
+    <app-header></app-header>
+    <div>
+      <h1>{{name}}</h1>
+    </div>
+    <app-footer></app-footer>
+  </div>
+  
 </template>
 
 <script setup>
   import {ref} from 'vue';
-
+  //the second way 
+  // import Footer from './components/header_footer/Footer.vue'
+  //and use <Footer/> to the template
   const name=ref("Hello world");
  
 
