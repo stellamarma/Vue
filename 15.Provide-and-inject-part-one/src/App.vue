d23efc055af46684175c9201eece8d5b6c8adfde
stellamarma <stellamarmat@gmail.com>
2025-01-23 12:11:04 +0200

    Create a slot to brand file to take what is in to app file

---
+++ 15.Provide-and-inject-part-one/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <app-header/>
+        <div class="container">
+            <Cars />
+            <hr/>
+            <button @click="updateCar">Update from parent</button>
+            <hr/>
+            <CarBrands>
+                <ul>
+                <li>Mazda</li>
+                <li>Honda</li>
+                <li>lopezz</li>
+                </ul>
+            </CarBrands>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { reactive,provide } from 'vue';
+    import Cars from './components/Cars/index.vue';
+    import CarBrands from './components/Cars/brads.vue'
+
+    const cars=reactive([
+        {model:'F9',brand:'Ferrari'},
+        {model:'911',brand:'Porsche'},
+        {model:'Tipo',brand:'Fiat'}
+    ])
+   
+
+    const updateCar=()=>{
+        cars[0].model='shakira'
+
+    }
+
+    provide('cars',{cars,updateCar})
+</script>
+
+<style>
+    body {
+        padding: 0;
+        margin: 0;
+        font-family: 'Robot',sans-serif;
+    }
+    .container {
+        min-height:84vh;
+        box-sizing: border-box;
+        padding: 20px;
+    }
+
+</style>
---
+++ 15.Provide-and-inject-part-one/src/components/Cars/brads.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="car_brands">
+        <h3>Car brads</h3>
+        <slot></slot>
+    </div>
+</template>
+<style>
+    .car_brands{
+        border:1px solid #2196f3;
+        padding:10px 20px;
+    }
+</style>
