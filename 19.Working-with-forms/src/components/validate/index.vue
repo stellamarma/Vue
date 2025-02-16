d3e0514420f6d291181983449a92eb5971fc3b78
stellamarma <stellamarmat@gmail.com>
2025-02-16 16:01:22 +0200

    Create a form

---
+++ 19.Working-with-forms/src/components/validate/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="mb-3">
+        <label for="name">Name</label>
+    </div>
+
+    <button class="btn btn-primary">
+      Submit
+    </button>
+</template>
+
+<script setup>
+</script>
