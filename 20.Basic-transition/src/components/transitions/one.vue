1738c91fe6c47c8a0d83c66f4ab2bbb328d14821
stellamarma <stellamarmat@gmail.com>
2025-02-23 15:16:21 +0200

    Add slide transition

--- 20.Basic-transition/src/components/transitions/one.vue
+++ 20.Basic-transition/src/components/transitions/one.vue
@@ -1,7 +1,6 @@
 <template>
     <transition>
-        <div class="p-3 mb-2 bg-success text-white"
-            v-if="data.display"
+        <div class="p-3 mb-2 bg-success text-white" v-if="data.display"
         >
             Hello
         </div>
@@ -13,8 +12,7 @@
     </button>
     <hr/>
     <transition>
-        <div class="p-3 mb-2 bg-warning text-white"
-            v-if="data.display2"
+        <div class="p-3 mb-2 bg-warning text-white" v-if="data.display2"
         >
             Hello
         </div>
@@ -24,6 +22,18 @@
     >
         Toggle display 2
     </button>
+    <hr/>
+    <transition name="slide">
+        <div class="p-3 mb-2 bg-warning text-white" v-if="data.display3"
+        >
+            Hello
+        </div>
+    </transition>
+    <button class="btn btn-primary" 
+        @click="data.display3 = !data.display3"
+    >
+        Toggle display 3
+    </button>
 </template>
 
 <script setup>
@@ -31,7 +41,8 @@
 
    const data = reactive({
         display:false,
-        display2:true
+        display2:true,
+        display3:true
    })
 </script>
 
@@ -42,10 +53,36 @@
     }
     .v-enter-active,
     .v-leave-active {
-        transition: 5s;
+        transition: 2s;
     }
     .v-enter-to,
     .v-leave-from {
         opacity: 1;
     }
+    /* for slide side */
+    .slide-enter-from{
+        transform: translateX(20p);
+        opacity: 0;
+    }
+    .slide-enter-active{
+        transition: all 1s ease;
+
+    }
+    .slide-enter-to{
+        opacity: 1;
+    }
+    .slide-leave-from {
+        opacity: 1;
+        background-color: red !important;
+    }
+    .slide-leave-active{
+        transition: all .3s ease;
+    }
+    .slide-leave-to {
+        background-color: black !important;
+        transform: translateX(100p);
+        opacity: 0;
+    }
+
+
 </style>
