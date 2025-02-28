377dfc5bf5d026d888675889d60e942782fa39ce
stellamarma <stellamarmat@gmail.com>
2025-02-28 12:36:14 +0200

    Add router

---
+++ 23.Installing-Router/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <app-header></app-header>
+        <div class="container">
+          <router-view/>
+          <!-- <home-component></home-component> -->
+          <!-- <articles-main></articles-main> -->
+          <!-- <article-comp></article-comp> -->
+          <!-- <contact-component></contact-component> -->
+        </div>
+</template>
+
+<script setup>
+  // import HomeComponent from './components/home.vue';
+  // import ArticlesMain from './components/articles/article.vue'
+  // import ArticleComp from './components/articles/index.vue'
+  // import ContactComponent from './components/contact/index.vue'
+</script>
---
+++ 23.Installing-Router/src/components/header/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+      <header
+        class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom"
+      >
+        <router-link
+          to="/"
+          class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
+        >
+          <span class="fs-4">Beardit.com</span>
+        </router-link>
+  
+        <ul class="nav nav-pills">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link" aria-current="page">Home</router-link>
+          </li>
+          <li class="nav-item"><router-link to="/articles" class="nav-link">Articles</router-link></li>
+          <li class="nav-item"><router-link to="/contact" class="nav-link">Contact</router-link></li>
+        </ul>
+      </header>
+    </div>
+  </template>
+  
+  <style>
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: "Roboto", sans-serif;
+  }
+  </style>
