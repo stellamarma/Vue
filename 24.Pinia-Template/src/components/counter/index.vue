0384b426cb8dcd7adcb87f6ffc85f300b0e9448d
stellamarma <stellamarmat@gmail.com>
2025-05-26 15:51:57 +0300

    Add mutations

--- 24.Pinia-Template/src/components/counter/index.vue
+++ 24.Pinia-Template/src/components/counter/index.vue
@@ -2,15 +2,23 @@
     <div class="px-4 py-5 my-5 text-center">
       <h1 class="display-5 fw-bold">Count</h1>
       <div class="col-lg-6 mx-auto">
-        <p class="lead mb-4">0</p>
+        <p class="lead mb-4">{{count}}</p>
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          <button type="button" class="btn btn-primary btn-lg px-4 gap-3">
+          <button type="button" class="btn btn-primary btn-lg px-4 gap-3" @click="store.counter++">
             +
           </button>
-          <button type="button" class="btn btn-outline-secondary btn-lg px-4">
+          <button type="button" class="btn btn-outline-secondary btn-lg px-4" @click="store.counter--">
             -
           </button>
         </div>
       </div>
     </div>
   </template>
+<script setup>
+  import { useCounterStore } from '@/store/counter';
+  import {computed} from 'vue';
+
+  const store= useCounterStore();
+
+  const count =computed(()=>store.counter)
+</script>
